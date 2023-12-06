9c4e04b31e57d08fcf0e5fea11abe81a1252b577
Aadith Joseph Mathew <aadith.mathew@msds.christuniversity,in>
2023-12-06 23:34:02 +0530

    Update

---
+++ script.js
@@ -0,0 +1,17 @@
+<div class="logo"></div>
+        
+<nav>
+    <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="about.html">About</a></li>
+        <li><a href="education.html">Education</a></li>
+        <li><a href="skills.html">Skills</a></li>
+        <li><a href="contact.html">Contact</a></li>
+    </ul>
+</nav>
+
+</header>
+
+<div style="margin-top: 300px ; margin-left: 300px;">
+<h1>Aadith Joseph Mathew</h1>
+</div>
